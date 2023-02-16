1f281486810d3dd170e1256737a2e440779de2c7
Muhammad Sufyan <muhammad.sufyan@apimatic.io>
2023-02-16 14:30:22 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='apimatic-requests-client-adapter',
-    version='0.1.1',
+    version='0.1.2',
     description='An adapter for requests client library consumed by the SDKs generated with APIMatic',
     long_description=long_description,
     long_description_content_type="text/markdown",
