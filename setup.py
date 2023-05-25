4e761be82370c598a137b8c01d91fc295ea28ad3
Muhammad Sufyan <sufyankhanrao123@gmail.com>
2023-05-25 13:00:48 +0500

    requests version upgraded to 2.31.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'apimatic-core-interfaces~=0.1.0',
-        'requests~=2.25',
+        'requests~=2.31',
         'cachecontrol~=0.12.6'
     ],
     tests_require=[
