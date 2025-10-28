9308ce222111806205c0b79ecad6c7b6c943943d
DESKTOP-10GD4VQ\Sufyan <muhammad.sufyan@apimatic.io>
2025-10-28 15:13:59 +0500

    fix(python-sdk): support requests.Session or custom http client via interface
    Allow SDK to accept either a plain requests.Session or a user-implemented
    HttpClientProvider when overriding the default HTTP client. Added handling
    for injected clients implementing the interface, enforcing session and timeout
    properties. Made the change non-breaking to maintain full backward compatibility
    with existing custom client implementations.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setup(
     name='apimatic-requests-client-adapter',
-    version='0.1.8',
+    version='0.1.9',
     description='An adapter for requests client library consumed by the SDKs generated with APIMatic',
     long_description=long_description,
     long_description_content_type="text/markdown",
